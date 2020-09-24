b58adbdc2af8cac94717fb5f2434d070c28621f6
techxpertmohit <techxpertmohit@users.noreply.github.com>
2020-09-24 15:12:00 +0530

    Init commit

---
+++ mongodb.js
@@ -0,0 +1,31 @@
+const {MongoClient, ObjectID} = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+    
+    
+    // db.collection('users').deleteMany({ 
+    //     age: 35
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    db.collection('tasks').deleteOne({
+        description: "Pot plants"
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+ 
+})
